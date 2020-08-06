286dfe33f07810ac25b840958614db30db137091
Clark David A <ClarkDavidA@JohnDeere.com>
2020-08-06 15:02:19 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,28 +1,30 @@
 
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[0];
+
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		//alert("You found a match!");
+		console.log("You found a match!");
+	} else {
+		//alert("Sorry, try again.");
+		console.log("Sorry, try again.");
+	}
+};
+
+function flipcard(cardId) {
+
+console.log("User flipped a " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+
 let i = 0;
 
-cardsInPlay.push(cardOne);
-//cardsInPlay.push(cardTwo);
-
 if (cardsInPlay.length === 2) {
-	while (i < 2) {
-		if (cardsInPlay[i] === "queen") {
-			  console.log("User flipped a queen");
-		} else {
-				console.log("User flipped a king");
-		  }
-		i = i + 1;
-	} 
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
+	checkForMatch();	
 } else {console.log("Draw a second card");}
 
-	
+};	
 
+flipcard(0);
+
+flipcard(2);
