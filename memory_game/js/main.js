d31ffd296bbab6b54e9bfd16e385a098fa3ad65d
Clark David A <ClarkDavidA@JohnDeere.com>
2020-08-07 07:52:12 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,26 @@
 
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+{
+	rank: "queen", 
+ 	suit: "hearts",
+ 	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen", 
+ 	suit: "diamonds",
+ 	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king", 
+ 	suit: "hearts",
+ 	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king", 
+ 	suit: "diamonds",
+ 	cardImage: "images/king-of-diamonds.png"
+}
+];
 let cardsInPlay = [];
 
 function checkForMatch() {
@@ -14,13 +35,18 @@ function checkForMatch() {
 
 function flipcard(cardId) {
 
-console.log("User flipped a " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped a " + cards[cardId].rank);
+console.log( cards[cardId].cardImage);
+console.log( cards[cardId].suit);
+
+cardsInPlay.push(cards[cardId].rank );
 
 let i = 0;
 
 if (cardsInPlay.length === 2) {
-	checkForMatch();	
+	checkForMatch();
+	cardsInPlay.pop();
+	cardsInPlay.pop();	
 } else {console.log("Draw a second card");}
 
 };	
